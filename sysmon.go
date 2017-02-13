97aa2fc6c2e830814ad98c5b47e55acc11520db3
dingerning <85099398@qq.com>
2017-02-13 09:03:02 +0800

    0213.1

--- sysmon.go
+++ sysmon.go
@@ -10,7 +10,7 @@ func main() {
 	// 	exec.Command("ps", "-ef"),
 	// 	exec.Command("grep", "redis"),
 	// 	exec.Command("grep", "-v", "grep"),
-	// 	exec.Command("/bin/sh", "-c", "sar -u|grep -v -E 'Linux|平均时间|cpu'|tail -n 3"),
+	// 	exec.Command("/bin/sh", "-c", "sar -u|grep -v -E 'Linux|平均时间|cpu|Average'|tail -n 3"),
 	// }
 	cpuStat()
 
@@ -18,7 +18,7 @@ func main() {
 
 //返回三个字段，时间，%iowait，%idle
 func cpuStat() []byte {
-	cmd := exec.Command("/bin/sh", "-c", "sar -u|grep -v -E 'Linux|平均时间|cpu'|tail -n 3")
+	cmd := exec.Command("/bin/sh", "-c", "sar -u|grep -v -E 'Linux|平均时间|cpu|Average'|tail -n 3")
 	cpustat, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
