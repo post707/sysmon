072da78fdcf797d46451e10a13b934746d08894c
dingerning <85099398@qq.com>
2017-02-14 16:00:41 +0800

    s

--- sysmon.go
+++ sysmon.go
@@ -7,7 +7,7 @@ import (
 
 //返回三个字段，时间，%iowait，%idle
 func cpuStat() []byte {
-	cmd := exec.Command("/bin/sh", "-c", `sar -u |tail -n 3|awk   'BEGIN {print "time  %iowait  %idle"} {print $1" "$6" "$8}`)
+	cmd := exec.Command("/bin/sh", "-c", `sar -u |tail -n 3|awk   'BEGIN {print "time %iowait %idle"} {print $1" "$6" "$8}'`)
 	cpustat, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
@@ -18,8 +18,15 @@ func cpuStat() []byte {
 
 //
 
-func memStat() {
+func memStat() []byte {
 
+	cmd := exec.Command("/bin/sh", "-c", `free -m|awk   ' {print $1" "$2" "$3}'`)
+	mem, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(mem))
+	return mem
 }
 
 func ioStat() {
