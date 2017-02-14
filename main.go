8a6990f0f1e54cdde7c33ca64b27310e216d222c
dingerning <85099398@qq.com>
2017-02-14 15:46:06 +0800

    s

--- main.go
+++ main.go
@@ -7,105 +7,9 @@ import (
 
 ////////////////////////////////////////////////////////
 //
-//错误检查
-//
-////////////////////////////////////////////////////////
-func checkError(err error, info string) (res bool) {
-
-	if err != nil {
-		fmt.Println(info + "  " + err.Error())
-		return false
-	}
-	return true
-}
-
-////////////////////////////////////////////////////////
-//
-//服务器端接收数据线程
-//参数：
-//      数据连接 conn
-//      通讯通道 messages
-//
-////////////////////////////////////////////////////////
-func Handler(conn net.Conn, messages chan string, clientip chan string) {
-
-	fmt.Println("connection is connected from ...", conn.RemoteAddr().String())
-
-	buf := make([]byte, 1024)
-	for {
-		lenght, err := conn.Read(buf)
-		if checkError(err, "Connection") == false {
-			conn.Close()
-			break
-		}
-		if lenght > 0 {
-			buf[lenght] = 0
-		}
-		//fmt.Println("Rec[",conn.RemoteAddr().String(),"] Say :" ,string(buf[0:lenght]))
-		reciveStr := string(buf[0:lenght])
-		messages <- reciveStr
-		ip := conn.RemoteAddr().String()
-		clientip <- ip
-
-	}
-
-}
-
-////////////////////////////////////////////////////////
-//
-//服务器发送数据的线程
-//
-//参数
-//      连接字典 conns
-//      数据通道 messages
-//
-////////////////////////////////////////////////////////
-func echoHandler(conns *map[string]net.Conn, messages chan string, clientip chan string) {
-
-	for {
-		msg := <-messages
-		clientip := <-clientip
-		fmt.Println("接收到的数据: ", msg)
-		for key, value := range *conns {
-			if clientip == key {
-				switch msg {
-				case "cpu":
-					fmt.Println("send data to", "ip", key)
-					sendmsg := cpuStat()
-					_, err := value.Write([]byte(sendmsg))
-					if err != nil {
-						fmt.Println(err.Error())
-						delete(*conns, key)
-					}
-				case "mem":
-					fmt.Println("send data to", "ip", key)
-					sendmsg := cpuStat()
-					_, err := value.Write([]byte(sendmsg))
-					if err != nil {
-						fmt.Println(err.Error())
-						delete(*conns, key)
-					}
-				case "io":
-					fmt.Println("send data to", "ip", key)
-					sendmsg := cpuStat()
-					_, err := value.Write([]byte(sendmsg))
-					if err != nil {
-						fmt.Println(err.Error())
-						delete(*conns, key)
-					}
-				}
-
-			}
-		}
-	}
-
-}
-
-////////////////////////////////////////////////////////
-//
 //启动服务器
 //参数
-//  端口 port
+//端口 port
 //
 ////////////////////////////////////////////////////////
 func StartServer(port string) {
@@ -114,19 +18,11 @@ func StartServer(port string) {
 	checkError(err, "ResolveTCPAddr")
 	l, err := net.ListenTCP("tcp", tcpAddr)
 	checkError(err, "ListenTCP")
-	//conns := make(map[string]net.Conn)
-	//messages := make(chan string, 10)
-	//clientip := make(chan string, 10)
-
-	// //启动服务器广播线程
-	// go echoHandler(&conns, messages, clientip)
 
 	for {
 		fmt.Println("Listening ...")
 		conn, err := l.Accept()
 		checkError(err, "Accept")
-		//conns[conn.RemoteAddr().String()] = conn
-		//启动一个新线程
 		buf := make([]byte, 1024)
 		lenght, err := conn.Read(buf)
 		if checkError(err, "Connection") == false {
@@ -136,36 +32,26 @@ func StartServer(port string) {
 		if lenght > 0 {
 			buf[lenght] = 0
 		}
-		//fmt.Println("Rec[",conn.RemoteAddr().String(),"] Say :" ,string(buf[0:lenght]))
 		reciveStr := string(buf[0:lenght])
-		// messages <- reciveStr
-		// ip := conn.RemoteAddr().String()
-		// clientip <- ip
-
+		fmt.Println("sendmsg to " + conn.RemoteAddr().String())
 		switch reciveStr {
 		case "cpu":
-
 			sendmsg := cpuStat()
 			_, err := conn.Write([]byte(sendmsg))
 			if err != nil {
 				fmt.Println(err.Error())
-
 			}
 		case "mem":
-
 			sendmsg := cpuStat()
 			_, err := conn.Write([]byte(sendmsg))
 			if err != nil {
 				fmt.Println(err.Error())
-
 			}
 		case "io":
-
 			sendmsg := cpuStat()
 			_, err := conn.Write([]byte(sendmsg))
 			if err != nil {
 				fmt.Println(err.Error())
-
 			}
 		}
 
@@ -176,4 +62,18 @@ func main() {
 
 	StartServer("6061")
 
+}
+
+////////////////////////////////////////////////////////
+//
+//错误检查
+//
+////////////////////////////////////////////////////////
+func checkError(err error, info string) (res bool) {
+
+	if err != nil {
+		fmt.Println(info + "  " + err.Error())
+		return false
+	}
+	return true
 }
